e5cfeab3dd40161c669c673481050322cf3ee7db
andrewp-as-is <russianidiot.github@gmail.com>
2020-06-30 20:05:11 +0300

    init

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='django-installed-apps-command',
-    version='2020.4.16',
-    install_requires=[
-        'Django',
-        'setuptools',
-    ],
-    packages=[
-        'django_installed_apps_command',
-        'django_installed_apps_command.management',
-        'django_installed_apps_command.management.commands',
-    ],
+    install_requires=open('requirements.txt').read().splitlines(),
+    packages=setuptools.find_packages()
 )
